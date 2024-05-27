f35c052804e5e9a15b133fd42b554d8b57a66e07
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-05-27 20:24:06 +0530

    added SpecialArray.java

--- Arrays/Java/SpecialArray.java
+++ Arrays/Java/SpecialArray.java
@@ -1,6 +1,6 @@
 /*
 Leetcode Question :
-https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/description/?envType=daily-question&envId=2024-05-27
+https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/description/
  */
 
 public class SpecialArray {
