8717871afa26f120ab878670ec1002a0f79fab0d
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-06-05 17:16:43 +0530

    added FirstAndLastOccurence.cpp

---
+++ Arrays/CPP/FirstAndLastOccurence.cpp
@@ -0,0 +1,48 @@
+/*
+Leetcode Question :
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+*/
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> result = {-1, -1};
+        if (nums.size() == 0) {
+            return result;
+        }
+
+        // Find the first occurrence
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result[0] = mid;
+                right = mid - 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        // Reset pointers to find the last occurrence
+        left = 0;
+        right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result[1] = mid;
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+};
