c18b3b009f293b3b6ee77a2bfc891be19a7dcb77
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-06-10 08:40:38 +0530

    added FindPeakElements.java

---
+++ Arrays/Java/FindPeakElement.java
@@ -0,0 +1,26 @@
+public static int findPeakElement(int[] nums) {
+        if(nums.length == 1) {
+            return 0;
+        }
+        int left = 1;
+        int right = nums.length - 2;
+        if(nums[0] > nums[1]) {
+            return 0;
+        }
+        else if (nums[nums.length - 1] > nums[nums.length - 2]) {
+            return nums.length - 1;
+        }
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]) {
+                return mid;
+            }
+            else if(nums[mid] > nums[mid+1]) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
