0cd9425d60f6f40353c99e79b9787f5e5251b389
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-06-14 08:24:24 +0530

    added LengthOfTheLastWord.java

---
+++ Strings/Java/LengthOfTheLastWord.java
@@ -0,0 +1,16 @@
+public class LengthOfTheLastWord {
+    public static int lengthOfLastWord(String s) {
+        // Trim any leading or trailing whitespace
+        s = s.trim();
+
+        // Find the last space after trimming
+        int lastSpaceIndex = s.lastIndexOf(' ');
+
+        // Return the length of the last word
+        return s.length() - lastSpaceIndex - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("Hello World"));     
+    }
+}
