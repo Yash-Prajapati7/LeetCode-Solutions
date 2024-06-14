99e81be845f26ba4e62deb3f6b0b019e42c435f8
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-06-14 08:38:03 +0530

    added LengthOfTheLastWord.cpp

---
+++ Strings/CPP/LengthOfTheLastWord.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int lengthOfLastWord(string str) {
+    // Trim trailing spaces
+    while (!str.empty() && str[str.length() - 1] == ' ') {
+        str.pop_back();
+    }
+
+    int length = 0;
+    for (int i = str.length() - 1; i >= 0; --i) {
+        if (str[i] == ' ') {
+            break;
+        }
+        length++;
+    }
+
+    // Return the length of the last word
+    return length;
+}
+
+int main() {
+    cout << lengthOfLastWord("   Hello World   ") << endl;
+    return 0;
+}
