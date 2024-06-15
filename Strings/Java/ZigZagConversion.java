05aad4021640eb1e2b45b6f7b0666243426a3913
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-06-15 08:14:22 +0530

    added ZigZagConversion.java

---
+++ Strings/Java/ZigZagConversion.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZigZagConversion {
+    public String convert(String s, int numRows) {
+        if (numRows <= 1 || s.length() <= numRows) {
+            return s;
+        }
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            rows.add(new StringBuilder());
+        }
+
+        int currentRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(currentRow).append(c);
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            currentRow = currentRow + goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        ZigZagConversion solution = new ZigZagConversion();
+        String s = "PAYPALISHIRING";
+        int numRows = 3;
+        System.out.println(solution.convert(s, numRows));
+    }
+}
