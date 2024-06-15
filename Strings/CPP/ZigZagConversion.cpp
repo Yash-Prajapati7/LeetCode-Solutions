851f2030d356a5f548f3773155f25e121e216f73
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-06-15 08:16:09 +0530

    added ZigZagConversion.cpp

---
+++ Strings/CPP/ZigZagConversion.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class ZigZagConversion {
+public:
+    string convert(const string& s, int numRows) {
+        if (numRows <= 1 || s.length() <= numRows) {
+            return s;
+        }
+
+        vector<string> rows(numRows);
+        int currentRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[currentRow] += c;
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        string result;
+        for (const string& row : rows) {
+            result += row;
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    ZigZagConversion solution;
+    string s = "PAYPALISHIRING";
+    int numRows = 3;
+    cout << solution.convert(s, numRows) << endl; 
+    return 0;
+}
