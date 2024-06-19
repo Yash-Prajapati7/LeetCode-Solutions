9f328c46991ff100a3bc09ea3166508dde11b16e
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-06-19 11:22:36 +0530

    added DeleteNode.java

---
+++ LinkedList/DeleteNode.java
@@ -0,0 +1,10 @@
+/*
+Leetcode Question :
+https://leetcode.com/problems/delete-node-in-a-linked-list/
+*/
+public class DeleteNode {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
