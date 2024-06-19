1cafc28afebe4ad3db2c85bcebca68f1fa61972e
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-06-19 11:11:48 +0530

    Updated SeparateOddEvenLinksInLL.java

--- LinkedList/SeparateOddEvenLinksInLL.java
+++ LinkedList/SeparateOddEvenLinksInLL.java
@@ -1,39 +1,54 @@
+/*
+Leetcode Question :
+https://leetcode.com/problems/odd-even-linked-list/
+*/
 public class SeparateOddEvenLinksInLL {
-    public static Node separate(Node head) {
-        Node odd = head;
-        Node evenHead = head.next;
-        Node even = head.next;
-        while(even.next != null && even != null) {
+    public static ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head; // Return early if the list is empty or has only one node.
+        }
+
+        ListNode odd = head;
+        ListNode evenHead = head.next;
+        ListNode even = evenHead;
+
+        while (even != null && even.next != null) {
             odd.next = odd.next.next;
             even.next = even.next.next;
             odd = odd.next;
             even = even.next;
         }
-        odd.next = evenHead;
+
+        // Connect the end of the odd list to the head of the even list
+        if (odd != null) {
+            odd.next = evenHead;
+        }
+
         return head;
     }
-    public static Node fromArray(int[] arr) {
+
+    public static ListNode fromArray(int[] arr) {
         if (arr == null || arr.length == 0) return null;
-        Node head = new Node(arr[0]);
-        Node temp = head;
+        ListNode head = new ListNode(arr[0]);
+        ListNode temp = head;
         for (int i = 1; i < arr.length; i++) {
-            temp.next = new Node(arr[i]);
+            temp.next = new ListNode(arr[i]);
             temp = temp.next;
         }
         return head;
     }
-    public static void print(Node head) {
-        Node temp = head;
+    public static void print(ListNode head) {
+        ListNode temp = head;
         while (temp != null) {
-            System.out.print(temp.data + "->");
+            System.out.print(temp.val + "->");
             temp = temp.next;
         }
         System.out.println("null");
     }
     public static void main(String[] args) {
-        Node head = null;
+        ListNode head = null;
         int [] nums = {2,3,1,4,5,6};
         head = fromArray(nums);
-        print(separate(head));
+        print(oddEvenList(head));
     }
 }
