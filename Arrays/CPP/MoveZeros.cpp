e08ed715bdb8b01e55de0e863d952a9ac5b81300
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-07-02 12:27:02 +0530

    Updated MoveZeros.cpp

--- Arrays/CPP/MoveZeros.cpp
+++ Arrays/CPP/MoveZeros.cpp
@@ -1,47 +1,20 @@
-/*
-Leetcode Question :
-https://leetcode.com/problems/move-zeroes/description/
-*/
-
-#include <iostream>
+// Leetcode Question : https://leetcode.com/problems/move-zeroes/
 #include <vector>
 
-using namespace std;
-
-vector<int> shiftZerosToEnd(vector<int>& nums) {
-    int nonZeroIndex = 0;
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] != 0) {
-            if (i != nonZeroIndex) {
-                // Swap non-zero element to the left
-                int temp = nums[i];
-                nums[i] = nums[nonZeroIndex];
-                nums[nonZeroIndex] = temp;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int nonZeroIndex = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                if (i != nonZeroIndex) {
+                    // Swap non-zero element to the left
+                    int temp = nums[i];
+                    nums[i] = nums[nonZeroIndex];
+                    nums[nonZeroIndex] = temp;
+                }
+                nonZeroIndex++;
             }
-            nonZeroIndex++;
         }
     }
-    return nums;
-}
-
-int main() {
-    vector<int> nums;
-    int val;
-    cout << "Keep Entering Integers (Enter -1 to exit)" << endl;
-    while (true) {
-        cin >> val;
-        if (val == -1) {
-            break;
-        }
-        nums.push_back(val);
-    }
-
-    cout << "Your modified array is: " << endl;
-    vector<int> modifiedArray = shiftZerosToEnd(nums);
-    for (int num : modifiedArray) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
+};
