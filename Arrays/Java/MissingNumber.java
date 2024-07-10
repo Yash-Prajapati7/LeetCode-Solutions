696c6c1d726852a3f32c212e69aba10ea6e7889f
Yash-Prajapati7 <157267869+Yash-Prajapati7@users.noreply.github.com>
2024-07-10 11:23:17 +0530

    added MissingNumber.java

---
+++ Arrays/Java/MissingNumber.java
@@ -0,0 +1,14 @@
+// Leetcode Question : https://leetcode.com/problems/missing-number/description/
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = (n * (n + 1)) / 2;
+        int actualSum = 0;
+        
+        for(int i = 0; i < n; i++) {
+            actualSum += nums[i];
+        }
+
+        return (expectedSum - actualSum);
+    }
+}
